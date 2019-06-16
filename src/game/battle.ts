d5514489551d46fd1e632a8f6219ff3139d3537a
Luna Hortin <luna@Lunas-MacBook-Air.local>
2019-06-16 15:32:01 +1000

    extract battle object

---
+++ src/game/battle.ts
@@ -0,0 +1,74 @@
+import { Creature } from './creature';
+import { MessageAction } from './messageBox';
+import { Game } from './game';
+
+export enum BattleResult {
+    WON,
+    LOST
+}
+
+export class Battle {
+    constructor(public game: Game, public enemy: Creature) {}
+
+    get player() {
+        return this.game.player;
+    }
+
+    doBattle = async (): Promise<BattleResult> => {
+        while (true) {
+            this.statusMessage();
+            await this.playerTurn();
+            if (this.battleFinished()) {
+                break;
+            }
+            this.enemyTurn();
+            if (this.battleFinished()) {
+                break;
+            }
+        }
+        return this.getBattleResult();
+    }
+
+    statusMessage = () => {
+        this.game.messageBox.showText(
+`You: ${this.player.name}
+HP: ${Math.floor(this.player.currentHp)}/${this.player.maxHp}`
+        );
+        this.game.messageBox.showText(
+`Enemy: ${this.enemy.name}
+HP: ${this.enemy.getHpFuzzyText()}`
+        );
+    }
+
+    playerTurn = async () => {
+        const messageActions: MessageAction[] = [];
+
+        for (const action of this.player.actions) {
+            messageActions.push({
+                text: action.name,
+                callback: () => action.action(this.enemy)
+            });
+        }
+
+        await this.game.messageBox.prompt(messageActions);
+    }
+
+    enemyTurn = () => {
+        this.enemy.enemyTurn();
+    }
+
+    battleFinished = () => {
+        return this.player.defeated || this.enemy.defeated;
+    }
+
+    getBattleResult = () => {
+        if (this.player.currentHp <= 0) {
+            return BattleResult.LOST;
+        }
+        if (this.enemy.currentHp <= 0) {
+            return BattleResult.WON;
+        }
+        throw new Error('Battle ended with neither win nor loss');
+    }
+
+}
--- src/game/game.ts
+++ src/game/game.ts
@@ -1,71 +1,36 @@
-import { MessageAction, MessageBox } from "src/game/messageBox";
-import { Creature, CreatureAction } from './creature';
+import { MessageBox } from "src/game/messageBox";
+import { Creature } from './creature';
 import { PuppetCreature } from './creatures/puppet';
 import { RatCreature } from './creatures/rat';
+import { Battle, BattleResult } from './battle';
 
 export class Game {
     player: Creature;
-    enemy: Creature;
+    battle: Battle;
 
     constructor(public messageBox: MessageBox) {
+        this.doGame().catch( err => alert(err) );
+    }
+
+    doGame = async () => {
         this.startupMessage();
         this.initData();
-        this.mainLoop().catch( err => alert(err) );
-    }
-
-    mainLoop = async () => {
-        while (true) {
-            this.statusMessage();
-            await this.playerTurn();
-            if (this.battleFinished()) {
-                break;
-            }
-            this.enemyTurn();
-            if (this.battleFinished()) {
-                break;
-            }
+        this.battle = new Battle(this, new RatCreature(this));
+        const result = await this.battle.doBattle();
+        if (result === BattleResult.WON) {
+            this.messageBox.showText('You won!');
+        } else {
+            this.messageBox.showText('You lost...');
         }
     }
 
-    playerTurn = async () => {
-        const messageActions: MessageAction[] = [];
-
-        for (const action of this.player.actions) {
-            messageActions.push({
-                text: action.name,
-                callback: () => action.action(this.enemy)
-            });
-        }
-
-        await this.messageBox.prompt(messageActions);
-    }
-
-    enemyTurn = () => {
-        this.enemy.enemyTurn();
-    }
-
-    battleFinished = () => {
-        return this.player.defeated || this.enemy.defeated;
-    }
 
     startupMessage = () => {
         this.messageBox.showText('Welcome to the game!');
     }
 
-    statusMessage = () => {
-        this.messageBox.showText(
-`You: ${this.player.name}
-HP: ${Math.floor(this.player.currentHp)}/${this.player.maxHp}`
-        );
-        this.messageBox.showText(
-`Enemy: ${this.enemy.name}
-HP: ${this.enemy.getHpFuzzyText()}`
-        );
-    }
-
     initData = () => {
         this.player = new PuppetCreature(this);
-        this.enemy = new RatCreature(this);
     }
 
 }
