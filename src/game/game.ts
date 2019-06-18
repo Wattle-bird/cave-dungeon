44dcf33e826e26d8719c11f747864f2a0748c178
Luna Hortin <luna@Lunas-MacBook-Air.local>
2019-06-18 16:41:43 +1000

    main menu

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,9 +1,9 @@
-import { MessageBox } from "src/game/messageBox";
+import { MessageBox, MessageAction } from "src/game/messageBox";
 import { Creature } from './creatures/creature';
 import { PuppetCreature } from './creatures/puppet';
-import { RatCreature } from './creatures/rat';
 import { Battle, BattleResult } from './battle';
 import { BearCreature } from './creatures/bear';
+import { RatCreature } from './creatures/rat';
 
 export class Game {
     player: Creature;
@@ -19,15 +19,28 @@ export class Game {
     doGame = async () => {
         this.startupMessage();
         this.initData();
-        this.battle = new Battle(this, new BearCreature(this));
-        const result = await this.battle.doBattle();
-        if (result === BattleResult.WON) {
-            this.messageBox.showText('You won!');
-        } else {
-            this.messageBox.showText('You lost...');
+        while (true) {
+            await this.doMainMenu();
         }
     }
 
+    doMainMenu = async () => {
+        const choices: MessageAction[] = [
+            {
+                text: 'Battle a rat',
+                value: this.doBattleRat
+            }, {
+                text: 'Battle a bear',
+                value: this.doBattleBear
+            }, {
+                text: 'Heal',
+                value: this.doHeal
+            }
+        ];
+
+        const choice = await this.messageBox.prompt(choices);
+        await choice();
+    }
 
     startupMessage = () => {
         this.messageBox.showText('Welcome to the game!');
@@ -37,4 +50,28 @@ export class Game {
         this.player = new PuppetCreature(this);
     }
 
+    doHeal = async () => {
+        this.player.currentHp = this.player.maxHp;
+        this.player.defeated = false;
+        this.messageBox.showText(`${this.player.name} is fully healed`);
+    }
+
+    doBattleRat = async () => {
+        this.battle = new Battle(this, new RatCreature(this));
+        await this.doBattle();
+    }
+
+    doBattleBear = async () => {
+        this.battle = new Battle(this, new BearCreature(this));
+        await this.doBattle();
+    }
+
+    doBattle = async () => {
+        const result = await this.battle.doBattle();
+        if (result === BattleResult.WON) {
+            this.messageBox.showText('You won!');
+        } else {
+            this.messageBox.showText('You lost...');
+        }
+    }
 }
