dad66d24c806c248f0806c50757459b4d0739934
Luna Hortin <luna@Lunas-MacBook-Air.local>
2019-06-29 15:23:28 +1000

    access game from the console

--- src/game/game.ts
+++ src/game/game.ts
@@ -12,6 +12,7 @@ export class Game {
     battle: Battle;
 
     constructor(public messageBox: MessageBox) {
+        (window as any).game = this;
         this.doGame().catch( err => {
             alert(err);
             throw err;
