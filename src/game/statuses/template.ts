08abfa4c331005db21b7cee1feb0b4460325d907
Luna Hortin <luna@Lunas-MacBook-Air.local>
2019-06-29 13:50:27 +1000

    template status

---
+++ src/game/statuses/template.ts
@@ -0,0 +1,19 @@
+import { Status } from "./status";
+import { Creature } from '../creatures/creature';
+import { Effect } from '../effect';
+import { Game } from '../game';
+
+class TemplateStatus implements Status {
+    name = 'TODO';
+
+    // constructor (private game: Game) {} // TODO
+
+    // doAction(user: Creature, target: Creature) {
+    //     // TODO
+    // }
+
+    // modifyOutgoingEffect(creature: Creature, effect: Effect) {
+    //     // TODO
+    //     return null;
+    // }
+}
